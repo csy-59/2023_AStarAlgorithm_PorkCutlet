0c8f77cb3c4938310b7435c9b65e963bacbb60ea
csy-59 <csy010509@naver.com>
2023-01-20 15:59:11 +0900

    [Fix] 연산의 최적화를 위해 2차원 배열로 변경

--- AStarAlgorithm/Assets/Scripts/MousePointToMove.cs
+++ AStarAlgorithm/Assets/Scripts/MousePointToMove.cs
@@ -5,15 +5,48 @@ using UnityEngine;
 
 public class MousePointToMove : MonoBehaviour
 {
+    private class EndNotReachable : System.Exception
+    {
+        private const string massage = "시작 지점과 목표 지점이 이어지지 않음";
+        public override string ToString()
+        {
+            return massage;
+        }
+    }
+
+    public struct Node
+    {
+        public int TotalCost => FromStart + ToEnd;
+        public int FromStart { get; set; }
+        public int ToEnd { get; set; }
+    }
+
+
     [SerializeField] private Camera mainCamera;
     [SerializeField] private TileCreater tileCreater;
     private Transform playerTransform;
     private float cameraHeight;
 
+    // 사용할 배열들
+    public Node[] Nodes { get; set; }
+    private bool[] isVisited;
+    private int mapSize;
+    private int width, height;
+
+    private readonly EndNotReachable endIsBlock = new EndNotReachable(true);
+    private readonly EndNotReachable endNotReachablek = new EndNotReachable(false);
+
     public void Start()
     {
         playerTransform = tileCreater.PlayerTransform;
         cameraHeight = mainCamera.transform.position.y;
+
+        width = tileCreater.Width;
+        height = tileCreater.Width;
+
+        mapSize = width * height;
+        Nodes = new Node[mapSize];
+        isVisited = new bool[mapSize];
     }
     public void Update()
     {
@@ -22,17 +55,55 @@ public class MousePointToMove : MonoBehaviour
         {
             try
             {
-                int startIndex = tileCreater.GetTileIndex(playerTransform.position);
+                var startIndex = tileCreater.GetTileIndex(playerTransform.position);
 
                 Vector3 point = mainCamera.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * cameraHeight);
-                int targetIndex = tileCreater.GetTileIndex(point);
+                var targetIndex = tileCreater.GetTileIndex(point);
+
+                if(tileCreater.Tiles[targetIndex.Item1][targetIndex.Item2].IsBlocked)
+                {
+                    Debug.Log("도달 할 수 없음");
+                    return;
+                }
 
                 Debug.Log($"시작 지점: {startIndex} 목표 지점: {targetIndex}");
+
             }
             catch (IndexOutOfRangeException e)
             {
                 Debug.Log($"맵 밖을 선택함");
             }
+        }
+    }
+
+    private bool SearchAStar(int start, int end)
+    {
+        ResetMap();
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(start);
+
+        int def = 0;
+        bool foundEnd = false;
+        while(queue.Count > 0)
+        {
+            int currentIndex = queue.Dequeue();
+            Nodes[currentIndex].FromStart = def;
+
+            // 시작 지점에서 끝 지점까지 도달했는지 확인
+            foundEnd = currentIndex == end;
+        }
+
+        if (!foundEnd)
+            throw endNotReachable;
+
+        return true;
+    }
+
+    private void ResetMap()
+    {
+        for(int i = 0; i< mapSize; ++i)
+        {
+            isVisited[i] = false;
         }
     }
 }
--- AStarAlgorithm/Assets/Scripts/TileCreater.cs
+++ AStarAlgorithm/Assets/Scripts/TileCreater.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 
 public class TileCreater : MonoBehaviour
 {
+    public struct NodeIndex
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
     [Header("==전체 타일 맵==")]
     [SerializeField] private int widthCount = 20;
     [SerializeField] private int heightCount = 20;
+    public int Width => widthCount;
+    public int Height => heightCount;
     
     [Header("\n==타일 정보==")]
     [SerializeField] private GameObject tilePrefab;
@@ -18,20 +26,27 @@ public class TileCreater : MonoBehaviour
     [SerializeField] private GameObject playPrefab;
     public Transform PlayerTransform { get; private set; }
 
-    private List<TileNode> tiles = new List<TileNode>();
-    public List<TileNode> Tiles => tiles;
-    private IndexOutOfRangeException outOfRangeException = 
+    private TileNode[][] tiles;
+    public TileNode[][] Tiles => tiles;
+    private readonly IndexOutOfRangeException outOfRangeException = 
         new IndexOutOfRangeException();
 
     public void Awake()
     {
+        tiles = new TileNode[widthCount][];
         for (int i = 0; i < widthCount; ++i)
         {
+            tiles[i] = new TileNode[widthCount];
             for (int j = 0; j < heightCount; ++j)
             {
                 GameObject newTile = Instantiate(tilePrefab);
                 newTile.transform.localPosition = new Vector3(tileWidth * i, 0f, tileHeight * j);
-                tiles.Add(newTile.GetComponent<TileNode>());
+                tiles[i][j] = newTile.GetComponent<TileNode>();
+
+                if (i == widthCount/2 && j == heightCount/2)
+                {
+                    tiles[i][j].SetToNormalTile();
+                }
             }
         }
 
@@ -39,23 +54,26 @@ public class TileCreater : MonoBehaviour
             new Vector3(tileWidth * widthCount / 2, 0f, tileHeight * heightCount / 2), 
             Quaternion.identity);
         PlayerTransform = player.transform;
-        tiles[widthCount / 2 + heightCount / 2].SetToNormalTile();
 
     }
 
-    public int GetTileIndex(Vector3 position)
+    private NodeIndex returnIndex;
+    public NodeIndex GetTileIndex(Vector3 position)
     {
         int xPosition = Mathf.RoundToInt(position.x / tileWidth);
         int zPosition = Mathf.RoundToInt(position.z / tileHeight);
-        int index = xPosition * widthCount + zPosition;
 
-        if(index >= tiles.Count || index < 0)
+        if(xPosition >= widthCount || xPosition < 0 ||
+            zPosition >= heightCount || zPosition < 0)
         {
             throw outOfRangeException;
         }
 
-        //tiles[index].GetComponent<MeshRenderer>().material.color = Color.red;
+        tiles[xPosition][zPosition].GetComponent<MeshRenderer>().material.color = Color.white;
 
-        return index;
+        returnIndex.X = xPosition;
+        returnIndex.Y = zPosition;
+
+        return returnIndex;
     }
 }
